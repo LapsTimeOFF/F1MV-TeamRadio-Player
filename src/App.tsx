aca7ef1d4d54ddabc89a465d96d04cfe685e6f96
Quentin (aka. LapsTime) <retine_baumier06@icloud.com>
2023-03-10 22:53:20 +0100

    push code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import nodeLogo from "./assets/node.svg"
-import { useState } from 'react'
-import './App.scss'
+import { useEffect, useState } from 'react';
+import './index.scss'
+import {Config, LiveTimingAPIGraphQL, discoverF1MVInstances} from 'npm_f1mv_api';
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+console.log(
+  "[App.tsx]",
+  `Hello world from Electron ${process.versions.electron}!`
+);
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://github.com/electron-vite/electron-vite-react" target="_blank">
-          <img src="./electron-vite.svg" className="logo" alt="Electron + Vite logo" />
-        </a>
-      </div>
-      <h1>Electron + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className="flex-center">
-        Place static files into the<code>/public</code> folder <img style={{ width: "5em" }} src={nodeLogo} alt="Node logo" />
-      </div>
-    </div>
-  )
+  const config: Config = {
+    host: "localhost",
+    port: 10101
+  };
+
+  let lastLength: number = 0;
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+
+      const {TeamRadio, TimingData, DriverList, SessionInfo} = await LiveTimingAPIGraphQL(config, ['TeamRadio', 'TimingData', 'DriverList', "SessionInfo"]);
+      // console.log({TeamRadio, TimingData, DriverList, SessionInfo});
+      if(TeamRadio.Captures.length !== lastLength) {
+        console.log(`New TeamRadio https://livetiming.formula1.com/static/${SessionInfo.Path}${TeamRadio.Captures[TeamRadio.Captures.length - 1].Path}`);
+
+        const audio = new Audio(`https://livetiming.formula1.com/static/${SessionInfo.Path}${TeamRadio.Captures[TeamRadio.Captures.length - 1].Path}`);
+        await audio.load();
+        audio.play();
+
+        lastLength = TeamRadio.Captures.length;
+      }
+
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (<p>Welcome! Just listen!</p>);
 }
 
-export default App
+export default App;
